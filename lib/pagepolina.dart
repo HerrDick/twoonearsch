33e2a57b0509f353dfd453421c94431a4e8ae616
vstrkvpolina <polinabarret000@gmail.com>
2023-10-20 19:01:14 +0300

    20.10.23

--- lib/pagepolina.dart
+++ lib/pagepolina.dart
@@ -1,24 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:twoonearsch/myhomepage.dart';
 
 class Polina extends StatelessWidget {
-  const Polina({Key? key}) : super(key: key);// Наши виджеты имеют один тип и состояние.
+  const Polina({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Push Me"),
+      body: ElevatedButton(
+        child: Text('Push the button'),
+        onPressed: () => Get.back(),
       ),
-      body: Center(
-        child: Text("Push the button"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Get.to(MyHomePage());
-        },
-    )
     );
-}
+  }
 }
