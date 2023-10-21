e96d5a75984265f2660271563266a3fa071f3802
Ma9ne1ius <145640861+Ma9ne1ius@users.noreply.github.com>
2023-10-21 10:39:34 +0500

    space

--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -12,32 +12,33 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Center(//spase
           child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-              onPressed: () {
-                Get.toNamed("/pagealesha");
-              },
-              child: Text("AleshaButton")),
-          ElevatedButton(
-              onPressed: () {
-                Get.toNamed("/pageanya");
-              },
-              child: Text("AnyaButton")),
-          ElevatedButton(
-              onPressed: () {
-                Get.toNamed("/pagepolina");
-              },
-              child: Text("PolinaButton")),
-          ElevatedButton(
-              onPressed: () {
-                Get.toNamed("/pagenikita");
-              },
-              child: Text("NikitaButton"))
-        ],
-      )),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed("/pagealesha");
+                  },
+                  child: Text("AleshaButton")),
+              ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed("/pageanya");
+                  },
+                  child: Text("AnyaButton")),
+              ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed("/pagepolina");
+                  },
+                  child: Text("PolinaButton")),
+              ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed("/pagenikita");
+                  },
+                  child: Text("NikitaButton"))
+          ],
+        )
+      ),
     );
   }
 }
